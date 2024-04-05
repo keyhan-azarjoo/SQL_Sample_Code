01e7ef39e6304e0c3422d5661d13972288030f52
keyhan azarjoo <keyhanazarjoo@gmail.com>
2024-04-05 10:51:59 +0100

    Update Usable_Code.sql

--- Usable_Code.sql
+++ Usable_Code.sql
@@ -28,3 +28,65 @@ CROSS APPLY OPENJSON(BulkColumn) WITH (
     stationId varchar(53),
     stationName varchar(max)
 ) AS station;
+
+
+-- =======================================================================================================================================================================================================
+
+
+
+-- Geting List Of Tables and their columns and replace null with 0
+-- For, Temp Table, All Table Names, All Column Name, 
+
+
+
+
+
+declare @columnValue as nvarchar(30)
+declare @columnValue2 as nvarchar(30)
+DECLARE @SQL NVARCHAR(MAX)
+SET @SQL = ''
+    
+	DECLARE cursorItem CURSOR FOR 
+		SELECT
+			TABLE_NAME
+		FROM #TempTables 
+	OPEN cursorItem;
+	FETCH NEXT FROM cursorItem INTO @columnValue;
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+	print (@columnValue)
+				DROP Table #temp
+				SELECT Distinct COLUMN_NAME INTO #temp
+				FROM [tripsmanagement].INFORMATION_SCHEMA.COLUMNS
+				WHERE TABLE_NAME = @columnValue
+				DECLARE cursorItem2 CURSOR FOR 
+					SELECT
+					COLUMN_NAME
+					FROM #temp 
+				OPEN cursorItem2;
+
+				WHILE @@FETCH_STATUS = 0
+				BEGIN
+		
+					print ('     '+@columnValue2)
+
+					SET @SQL = 'update [tripsmanagement].[trips].[' + @columnValue + '] set [' + @columnValue2 + '] = 0 where [' + @columnValue2 + '] is null'
+					EXEC(@SQL)
+					--print(@SQL)
+					print('				DONE')
+
+				FETCH NEXT FROM cursorItem2 INTO @columnValue2;
+
+				END
+				CLOSE cursorItem2;
+				DEALLOCATE cursorItem2;
+		FETCH NEXT FROM cursorItem INTO @columnValue;
+		END
+		CLOSE cursorItem;
+
+		DEALLOCATE cursorItem
+
+
+-- =======================================================================================================================================================================================================
+
